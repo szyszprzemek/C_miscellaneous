6c38d5c68794373d77974d6030c8118cfb79a941
szyszprzemek <47602880+szyszprzemek@users.noreply.github.com>
2023-08-21 20:06:18 +0200

    Update two_complement.c

--- two_complement.c
+++ two_complement.c
@@ -3,28 +3,48 @@
 #include <string.h>
 #include <stdbool.h>
 
-uint16_t value1 = 0xFFFE;
+/* This code checks for two compl value and converts it */
+
+uint16_t value1 = 0x104;
 uint16_t value2 = 0xFEFC;
 uint16_t value3 = 0xF5B2;
 uint16_t value4 = 0xD638;
 
-/* This code checks for two compl value */
-
-int main()
-{
-    int16_t result1, result2, result3, result4;
-    
-    memcpy(&result1, &value1, sizeof(value1));
-    memcpy(&result2, &value2, sizeof(value2));
-    memcpy(&result3, &value3, sizeof(value3));
-    memcpy(&result4, &value4, sizeof(value4));
-    
-    printf("For %d two compl: %d\n", value1, result1/32);
-    printf("For %d two compl: %d\n", value2, result2/32);
-    printf("For %d two compl: %d\n", value3, result3/32);
-    printf("For %d two compl: %d\n", value4, result4/32);
-    
-    return 0;
+bool isTwoCompl(uint16_t value){
+    return (0x8000 & value);
 }
 
+uint16_t removeMSB(uint16_t value){
+    return (0x7FFF & value);
+}
+
+int16_t converter(uint16_t value){
+    int16_t result;
+    
+    if(isTwoCompl(value))
+    {
+        result = (int16_t)(removeMSB(~value)/32);
+    }
+    else
+    {
+        result = (int16_t)(value/32);
+    }
+    return result;
+}
+
+int main()
+{   
+    int16_t result1, result2, result3, result4;
+    
+    printf("Is %d two compl: %d\n", value1, isTwoCompl(value1));
+    printf("Is %d two compl: %d\n", value2, isTwoCompl(value2));
+    printf("Is %d two compl: %d\n", value3, isTwoCompl(value3));
+    printf("Is %d two compl: %d\n", value4, isTwoCompl(value4));
+
+    printf("Is %d two compl: %d\n", value1, converter(value1));
+    printf("Is %d two compl: %d\n", value2, converter(value2));
+    printf("Is %d two compl: %d\n", value3, converter(value3));
+    printf("Is %d two compl: %d\n", value4, converter(value4));
+    return 0;
+}
 
